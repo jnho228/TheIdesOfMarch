04cabd076b93c3a750ae3a3c2fbc6f86c9e81750
Nick Howard <james.nicholas.howard@gmail.com>
2020-12-29 12:47:34 +0900

    Tutorial Restyling + App Icon

--- Assets/Scripts/TutorialGUI.cs
+++ Assets/Scripts/TutorialGUI.cs
@@ -6,11 +6,20 @@ using UnityEngine.SceneManagement;
 
 public class TutorialGUI : MonoBehaviour
 {
+    public Animator screenTransistion;
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            SceneManager.LoadScene(2);
+            StartCoroutine(LoadLevel());
         }
+    }
+
+    IEnumerator LoadLevel()
+    {
+        screenTransistion.Play("Scene Close");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(2);
     }
 }
