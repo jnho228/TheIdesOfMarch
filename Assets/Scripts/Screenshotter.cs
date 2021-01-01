99a068c3a795f061fc9951f975b05b3d3bdd076d
Nick Howard <james.nicholas.howard@gmail.com>
2021-01-02 02:16:10 +0900

    Screenshot Tool + Sprite Updates
    Added an in-editor only screenshotter that I'll probably use in future projects + made the sprites more "Rome-like."

---
+++ Assets/Scripts/Screenshotter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshotter : MonoBehaviour
+{
+    private void Update()
+    {
+        // For debug use only, so check if it's the editor or not.
+        // Actually should probably do a start function and if it isn't the editor then I should auto delete this gameobject....... but meh. We'll see! 
+        if (Input.GetKeyDown(KeyCode.G) && Application.isEditor)
+        {
+            string fileName = "Screen Shot " + System.DateTime.Now.Year + "-" + System.DateTime.Now.Month + "-" + System.DateTime.Now.Day + " at " + System.DateTime.Now.Hour + "." + System.DateTime.Now.Minute + "." + System.DateTime.Now.Second + ".png";
+            ScreenCapture.CaptureScreenshot(fileName);
+
+            Debug.Log("Saving " + fileName);
+        }
+    }
+}
